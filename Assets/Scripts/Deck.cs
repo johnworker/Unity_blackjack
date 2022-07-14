777b08b53a9391a62ae45ca240d383636ae65546
johnworker <78348941+johnworker@users.noreply.github.com>
2022-07-15 07:24:11 +0800

    新增程式檔和添加內容物

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    public Sprite[] cardSprites;
+    int[] cardValues = new int[53];
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
