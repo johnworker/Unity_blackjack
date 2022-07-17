a48eea72ec030499bef0a96820f3c482a61f1192
johnworker <78348941+johnworker@users.noreply.github.com>
2022-07-17 12:59:28 +0800

    添加程式碼

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // 此腳本適用於玩家和發牌員
+
+    // 取得其他腳本
+    public Card cardScript;
+    public Deck deckScript;
+
+    // 全部的數值取決於 玩家/發牌員 的手牌
+    public int handValue = 0;
+
+    // 投注金錢
+    private int money = 1000;
     void Start()
     {
         
