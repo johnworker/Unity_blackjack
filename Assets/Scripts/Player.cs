e2ba2f8d565490bdae0f4096ecec8c57a6f7b215
johnworker <78348941+johnworker@users.noreply.github.com>
2022-07-20 09:25:39 +0800

    添加程式碼

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // 此腳本適用於玩家和發牌員
+    // 此腳本適用於玩家和莊家
 
     // 取得其他腳本
     public Card cardScript;
     public Deck deckScript;
 
-    // 全部的數值取決於 玩家/發牌員 的手牌
+    // 全部的數值取決於 玩家/莊家 的手牌
     public int handValue = 0;
 
     // 投注金錢
@@ -22,14 +22,21 @@ public class Player : MonoBehaviour
     // 下一張要翻牌的索引
     public int cardIndex = 0;
     public int aceCount = 0;
+
+    // 追蹤 1 到 11 轉換的 王牌
+    List<Card> aceList = new List<Card>();
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // 發牌給玩家/莊家 的手中
+    public int GetCard()
     {
-        
+        // 取得撲克牌，用發牌將圖像和價值分配給桌子上的卡
+        int cardValue = deckScript.DealCard(hand[cardIndex].GetComponent<Card>());
+        // 展示撲克牌在遊戲螢幕
+        hand[cardIndex].GetComponent<Renderer>().enabled = true;
+        // 將撲克牌面值加到手牌總數中
     }
 }
